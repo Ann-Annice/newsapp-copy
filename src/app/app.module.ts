c85dd31a8bfe55ddad543773f2f991e0a5776901
Ann-Annice <annannice98@gmail.com>
2022-10-10 20:36:03 +0530

    newsappcopy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsappcopyComponent } from './newsappcopy/newsappcopy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsappcopyComponent
   ],
   imports: [
     BrowserModule,
